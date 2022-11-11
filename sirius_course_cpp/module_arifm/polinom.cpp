8f69ef103181236890b0059374a87df1c9f2966c
Yaroslav-Muravev <116451332+Yaroslav-Muravev@users.noreply.github.com>
2022-11-11 15:19:32 +0300

    Delete sirius_course_cpp directory

--- sirius_course_cpp/module_arifm/polinom.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << 1023456789876543201 << endl;
-    return 0;
-}
--- sirius_course_cpp/module_arifm/strange_str.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << 1+1 << " + " << 1+1 << " = " << 4 << endl;
-    return 0;
-}
